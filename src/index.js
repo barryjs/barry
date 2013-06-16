835ace08b9c9227198812d155c363cda15e18a76
Stefan Thomas <justmoon@members.fsf.org>
2013-06-16 11:16:35 +0200

    Add accessible dirname property for finding Barry base path in Node.js.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+var path = require('path');
+
 var Barry = module.exports = require('./barry').Barry;
 var Server = require('./server').Server;
 var ScalarService = require('./services/scalar').ScalarService;
@@ -7,3 +9,6 @@ var DictionaryService = require('./services/dictionary').DictionaryService;
 Barry.Server = Server;
 Barry.ScalarService = ScalarService;
 Barry.DictionaryService = DictionaryService;
+
+// Easy way to determine actual Barry installation directory
+Barry.dirname = path.resolve(__dirname, "..");
